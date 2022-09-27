a82dee14fd43036391c90a534d265aa3cec63480
boxmein <wsdf294@gmail.com>
2022-09-28 01:38:14 +0300

    bump

--- shell/path.zsh
+++ shell/path.zsh
@@ -63,4 +63,9 @@ if [[ -d /usr/local/Cellar/john-jumbo/1.9.0/share/john ]]; then
   paths+=(/usr/local/Cellar/john-jumbo/1.9.0/share/john)
 fi
 
+if [[ -d /usr/local/go/bin ]]; then
+  paths+=(/usr/local/go/bin)
+fi
+
+
 export PATH="${(j/:/)paths}:$PATH"
