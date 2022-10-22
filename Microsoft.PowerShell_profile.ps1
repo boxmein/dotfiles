53a08c9cdeb1ca971ad4a41454a330bfc40a9da6
Johannes Kadak <wsdf294@gmail.com>
2022-10-22 09:32:39 +0300

    ps profile update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,42 +1,68 @@
 # boxmein's powershell config 
 # ps core 7.2.2 || microsoft ps 5.0 something
 
+chcp 65001
+
 #region Environment add-ons
 
 $env:PYTHONIOENCODING = "utf-8"
+$env:EDITOR = "code.cmd"
+$env:VISUAL = "code.cmd"
+$env:ALTERNATIVE_EDITOR = "notepad"
+$env:PGCLIENTENCODING = "utf-8"
 
 #endregion
 
-#region ALIASES
+${function:~} = { Set-Location ~ }
 
-set-alias -Option AllScope -Name cat        -Value get-content
-set-alias -Option AllScope -Name cd         -Value set-location
-set-alias -Option AllScope -Name clear      -Value clear-host
-set-alias -Option AllScope -Name cp         -Value copy-item
-set-alias -Option AllScope -Name h          -Value get-history
-set-alias -Option AllScope -Name history    -Value get-history
-set-alias -Option AllScope -Name kill       -Value stop-process
-set-alias -Option AllScope -Name lp         -Value out-printer
-set-alias -Option AllScope -Name mount      -Value new-mshdrive
-set-alias -Option AllScope -Name mv         -Value move-item
-set-alias -Option AllScope -Name popd       -Value pop-location
-set-alias -Option AllScope -Name ps         -Value get-process
-set-alias -Option AllScope -Name pushd      -Value push-location
-set-alias -Option AllScope -Name pwd        -Value get-location
-set-alias -Option AllScope -Name r          -Value invoke-history
-set-alias -Option AllScope -Name rm         -Value remove-item
-set-alias -Option AllScope -Name rmdir      -Value remove-item
-set-alias -Option AllScope -Name echo       -Value write-output
-set-alias -Option AllScope -Name rd         -Value remove-item
-set-alias -Option AllScope -Name set        -Value set-variable
+#region Aliases
+# Dot dot
+${function:Set-ParentLocation} = { Set-Location .. }; 
+Set-Alias ".." Set-ParentLocation
+${function:...} = { Set-Location ..\.. }
+${function:....} = { Set-Location ..\..\.. }
+${function:.....} = { Set-Location ..\..\..\.. }
+${function:......} = { Set-Location ..\..\..\..\.. }
 
+
+Set-Alias -Option AllScope -Name cat        -Value get-content
+Set-Alias -Option AllScope -Name cd         -Value set-location
+Set-Alias -Option AllScope -Name clear      -Value clear-host
+Set-Alias -Option AllScope -Name cp         -Value copy-item
+Set-Alias -Option AllScope -Name h          -Value get-history
+Set-Alias -Option AllScope -Name history    -Value get-history
+Set-Alias -Option AllScope -Name kill       -Value stop-process
+Set-Alias -Option AllScope -Name lp         -Value out-printer
+Set-Alias -Option AllScope -Name mount      -Value new-mshdrive
+Set-Alias -Option AllScope -Name mv         -Value move-item
+Set-Alias -Option AllScope -Name popd       -Value pop-location
+Set-Alias -Option AllScope -Name ps         -Value get-process
+Set-Alias -Option AllScope -Name pushd      -Value push-location
+Set-Alias -Option AllScope -Name pwd        -Value get-location
+Set-Alias -Option AllScope -Name r          -Value invoke-history
+Set-Alias -Option AllScope -Name rm         -Value remove-item
+Set-Alias -Option AllScope -Name rmdir      -Value remove-item
+Set-Alias -Option AllScope -Name echo       -Value write-output
+Set-Alias -Option AllScope -Name rd         -Value remove-item
+Set-Alias -Option AllScope -Name set        -Value set-variable
+Set-Alias -Option AllScope -Name nslookup   -Value Resolve-DnsName
+
+Set-Alias time Measure-Command
+${function:sv} = { Set-Location D:\Projects\Calidity\ }
+${function:svctl} = { Set-Location D:\Projects\Calidity\controller }
+${function:svdev} = { Set-Location D:\Projects\Calidity\dev }
+${function:svsf} = { Set-Location D:\Projects\Calidity\dev\frontend }
+${function:dl} = { Set-Location ~\Downloads }
 #endregion
 
 ##region MORE COMPLEX ALIASES
 
+function dig {
+  Resolve-DnsName -Type $args[0] -Name $args[1]
+}
 
 function settings {
-  notepad "$env:USERPROFILE\dotfiles\Microsoft.PowerShell_profile.ps1"
+  code.cmd "$env:USERPROFILE\dotfiles\Microsoft.PowerShell_profile.ps1"
 }
 
 function help {
@@ -71,7 +97,154 @@ function gp {
   git push $args
 }
 
+function which($name) { 
+  Get-Command $name -ErrorAction SilentlyContinue | Select-Object Definition
+}
+function touch($file) {
+  "" | Out-File $file -Encoding ASCII
+}
+
+function dbc {
+  $env:PGPASSWORD = "postgres"
+  psql -U postgres -h localhost -d postgres 
+}
+
+# Sudo
+function sudo() {
+  if ($args.Length -eq 1) {
+    start-process $args[0] -verb "runAs"
+  }
+  if ($args.Length -gt 1) {
+    start-process $args[0] -ArgumentList $args[1..$args.Length] -verb "runAs"
+  }
+}
+
+
 #endregion
+
+#region bla
+function Edit-Hosts {
+  Invoke-Expression "sudo $(if($null -ne $env:EDITOR) { $env:EDITOR } else { 'notepad' }) $env:windir\system32\drivers\etc\hosts"
+}
+function Edit-Profile {
+  Invoke-Expression "$(if($null -ne $env:EDITOR)  {$env:EDITOR } else { 'notepad' }) $profile"
+}
+function Restart-Powershell {
+  $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+  $newProcess.Arguments = "-nologo";
+  [System.Diagnostics.Process]::Start($newProcess);
+  exit
+}
+# Sound Volume
+function Get-SoundVolume {
+  <#
+  .SYNOPSIS
+  Get audio output volume.
+  .DESCRIPTION
+  The Get-SoundVolume cmdlet gets the current master volume of the default audio output device. The returned value is an integer between 0 and 100.
+  .LINK
+  Set-SoundVolume
+  .LINK
+  Set-SoundMute
+  .LINK
+  Set-SoundUnmute
+  .LINK
+  https://github.com/jayharris/dotfiles-windows/
+  #>
+  [math]::Round([Audio]::Volume * 100)
+}
+function Set-SoundVolume([Parameter(mandatory = $true)][Int32] $Volume) {
+  <#
+  .SYNOPSIS
+  Set audio output volume.
+  .DESCRIPTION
+  The Set-SoundVolume cmdlet sets the current master volume of the default audio output device to a value between 0 and 100.
+  .PARAMETER Volume
+  An integer between 0 and 100.
+  .EXAMPLE
+  Set-SoundVolume 65
+  Sets the master volume to 65%.
+  .EXAMPLE
+  Set-SoundVolume -Volume 100
+  Sets the master volume to a maximum 100%.
+  .LINK
+  Get-SoundVolume
+  .LINK
+  Set-SoundMute
+  .LINK
+  Set-SoundUnmute
+  .LINK
+  https://github.com/jayharris/dotfiles-windows/
+  #>
+  [Audio]::Volume = ($Volume / 100)
+}
+function Set-SoundMute {
+  <#
+  .SYNOPSIS
+  Mote audio output.
+  .DESCRIPTION
+  The Set-SoundMute cmdlet mutes the default audio output device.
+  .LINK
+  Get-SoundVolume
+  .LINK
+  Set-SoundVolume
+  .LINK
+  Set-SoundUnmute
+  .LINK
+  https://github.com/jayharris/dotfiles-windows/
+  #>
+  [Audio]::Mute = $true
+}
+function Set-SoundUnmute {
+  <#
+  .SYNOPSIS
+  Unmote audio output.
+  .DESCRIPTION
+  The Set-SoundUnmute cmdlet unmutes the default audio output device.
+  .LINK
+  Get-SoundVolume
+  .LINK
+  Set-SoundVolume
+  .LINK
+  Set-SoundMute
+  .LINK
+  https://github.com/jayharris/dotfiles-windows/
+  #>
+  [Audio]::Mute = $false
+}
+
+# Reload the $env object from the registry
+function Restore-Environment {
+  $locations = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
+  'HKCU:\Environment'
+
+  $locations | ForEach-Object {
+    $k = Get-Item $_
+    $k.GetValueNames() | ForEach-Object {
+      $name = $_
+      $value = $k.GetValue($_)
+      Set-Item -Path Env:\$name -Value $value
+    }
+  }
+
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+}
+
+# Set a permanent Environment variable, and reload it into $env
+function Set-SystemEnvironment([String] $variable, [String] $value) {
+  Set-ItemProperty "HKCU:\Environment" $variable $value
+  # Manually setting Registry entry. SetEnvironmentVariable is too slow because of blocking HWND_BROADCAST
+  #[System.Environment]::SetEnvironmentVariable("$variable", "$value","User")
+  Invoke-Expression "`$env:${variable} = `"$value`""
+}
+
+function Prepend-EnvPath([String]$path) { $env:PATH = $env:PATH + ";$path" }
+function Prepend-EnvPathIfExists([String]$path) { if (Test-Path $path) { Prepend-EnvPath $path } }
+function Append-EnvPath([String]$path) { $env:PATH = $env:PATH + ";$path" }
+function Append-EnvPathIfExists([String]$path) { if (Test-Path $path) { Append-EnvPath $path } }
+
+#endregion
+
 
 #region Plugins
 
@@ -96,7 +269,7 @@ if (Test-Path($ChocolateyProfile)) {
 #
 # prompt: oh-my-posh
 # 
-$env:PATH += ";$env:LOCALAPPDATA\Programs\oh-my-posh\bin\"
+Append-EnvPathIfExists "$env:LOCALAPPDATA\Programs\oh-my-posh\bin\"
 oh-my-posh init pwsh --config $env:LOCALAPPDATA\Programs\oh-my-posh\themes\powerlevel10k_lean.omp.json | Invoke-Expression
 #endregion
 
