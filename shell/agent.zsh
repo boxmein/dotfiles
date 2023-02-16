32eba66307aa9e84c32930b75fa2cfcb3d32b430
boxmein <wsdf294@gmail.com>
2023-02-16 22:49:59 +0200

    also log which agent is in use

--- shell/agent.zsh
+++ shell/agent.zsh
@@ -6,10 +6,12 @@ _use_1password_ssh_agent() {
 	if [[ -S ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ]]; then
 		export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 	fi
+	echo "Using 1Password SSH agent"
 }
 
 _use_gpg_ssh_agent() {
 	export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+	echo "Using GPG SSH agent"
 }
 
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
