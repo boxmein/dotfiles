19dceeddfa4d6d9f81088acabbd144c7432557b5
boxmein <wsdf294@gmail.com>
2023-03-14 13:12:07 +0200

    update brewfile

--- nyx.nix
+++ nyx.nix
@@ -8,4 +8,38 @@
   environment.systemPackages = with pkgs; [
     discord
   ];
+
+  # environment.systemPath = [ ... ];
+  # environment.shellAliases = { ... };
+  # environment.profiles  ???;
+  # environment.launchDaemons
+  # environment.launchAgents
+  # environment.variables = { EDITOR = "nvim" };
+
+  homebrew = {
+    enable = true;
+
+    brews = [
+      
+    ];
+
+    casks = [
+      
+    ];
+
+    taps = [];
+
+    # masApps = [...]; (Installing from Mac App Store)
+  };
+
+  networking.hostName = "nyx";
+  networking.computerName = "nyx";
+  networking.localHostName = "nyx.local";
+
+
+  # programs.gpg
+
+
+  # services
+  services.emacs.enable = true;
 }
