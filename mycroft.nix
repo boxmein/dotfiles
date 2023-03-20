605453401c53d2c4dd5cc19a0aa80a438267d4bc
boxmein <wsdf294@gmail.com>
2023-03-20 08:22:23 +0200

    update mycroft.nxi

--- mycroft.nix
+++ mycroft.nix
@@ -3,4 +3,43 @@
     shell = pkgs.zsh;
   };
   home-manager.users.johannes = import ./home.nix;
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    discord
+  ];
+
+  # environment.systemPath = [ ... ];
+  # environment.shellAliases = { ... };
+  # environment.profiles  ???;
+  # environment.launchDaemons
+  # environment.launchAgents
+  # environment.variables = { EDITOR = "nvim" };
+
+  homebrew = {
+    enable = true;
+
+    brews = [
+
+    ];
+
+    casks = [
+
+    ];
+
+    taps = [];
+
+    # masApps = [...]; (Installing from Mac App Store)
+  };
+
+  networking.hostName = "mycroft";
+  networking.computerName = "mycroft";
+  networking.localHostName = "mycroft.local";
+
+
+  # programs.gpg
+
+
+  # services
+  services.emacs.enable = true;
 }
