16de27891c1185c2a4b87cb12e392ca98eef2cb9
boxmein <wsdf294@gmail.com>
2023-04-27 19:58:13 +0300

    simplify the services and progs blocks

--- configuration.nix
+++ configuration.nix
@@ -53,62 +53,32 @@
     LC_TIME = "et_EE.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "ee";
-    xkbVariant = "";
-  };
 
   # Configure console keymap
   console.keyMap = "et";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.johannes = {
     isNormalUser = true;
     description = "johannes";
     extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
     packages = with pkgs; [
-      firefox
-      kate
-      _1password-gui
-      gnupg
-      pinentry
-      tailscale
-      fprintd
-      git
-      neovim
-      zsh
-    #  thunderbird
+      discord
+        firefox
+        _1password-gui
+        gnupg
+        pinentry
+        pinentry-curses
+        exa
+        google-cloud-sdk
+        #  thunderbird
     ];
   };
 
@@ -118,31 +88,47 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-     tailscale
-     gnupg
-     pinentry-curses
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    wget
   ];
 
-  # tailscale
-  services.tailscale.enable = true;
+  services = {
+    printing.enable = true;
+    tailscale.enable = true;
+    openssh.enable = true;
+    flatpak.enable = true;
+    xserver = {
+      layout = "ee";
+      xkbVariant = "";
+      enable = true;
+      displayManager.sddm.enable = true;
+      desktopManager.plasma5.enable = true;
+    };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-    pinentryFlavor = "curses";
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
   };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  services.flatpak.enable = true;
+  programs = {
+    mtr.enable = true;
+    git.enable = true;
+    zsh.enable = true;
+    neovim.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+      pinentryFlavor = "curses";
+    };
+  };
 
 
   # Open ports in the firewall.
