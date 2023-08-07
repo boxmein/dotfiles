e8f6653618fffa131f65f357aa61f66fc111f62c
boxmein <wsdf294@gmail.com>
2023-08-07 14:22:06 +0300

    updates

--- nix/computers/trifle.nix
+++ nix/computers/trifle.nix
@@ -294,7 +294,7 @@
   systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
 
   # This is required so that pod can reach the API server (running on port 6443 by default)
-  services.k3s.enable = true;
+  services.k3s.enable = false;
   services.k3s.role = "server";
   services.k3s.extraFlags = toString [
     # "--kubelet-arg=v=4" # Optionally add additional args to k3s
